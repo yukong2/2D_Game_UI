fd4465f44ac549ebfaee8a3576d57c7ddf4480f2
yukong2 <rladbqls0212@gmail.com>
2023-08-02 15:51:07 +0900

    aa
    bb

---
+++ Assets/Scripts/MP_BarControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MP_BarControl : MonoBehaviour
+{
+    [SerializeField]
+    private Slider mpbar;
+
+    private float maxMp = 100;
+    private float curMp = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mpbar.value = (float)curMp / (float)maxMp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+            curMp -= 10;
+        HandleHP();
+    }
+
+    private void HandleHP()
+    {
+        mpbar.value = (float)curMp / (float)maxMp;
+    }
+}
