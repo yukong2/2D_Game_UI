e46e2f76da9427c5e651d624a1f795d4491bdcbb
yukong2 <rladbqls0212@gmail.com>
2023-08-11 03:35:13 +0900

    update1
    Monster HP Bar

---
+++ Assets/Scripts/Boss_HPControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss_HPControl : MonoBehaviour
+{
+    [SerializeField]
+    private Slider hpbar;
+
+    private float maxHp = 100;
+    private float curHp = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpbar.value = (float)curHp / (float)maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void HandleHP()
+    {
+        hpbar.value = (float)curHp / (float)maxHp;
+    }
+}
